c1bc3995cc183154cce87ad82a1e4021e0f11379
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-06-09 12:14:50 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -43,7 +43,7 @@
                   <th><?php echo $dato->apellido_paterno ?></th>
                   <th><?php echo $dato->apellido_materno ?></th>
                   <th> <a href="<?php base_url('/index.php/editar') ?>" class="btn btn-sm btn-warning">Editar</a> </th>
-                  <th> <a href="<?php base_url('/index.php/eliminar') ?>" class="btn btn-sm btn-danger">Editar</a> </th>
+                  <th> <a href="<?php base_url('/index.php/eliminar') ?>" class="btn btn-sm btn-danger">Eliminar</a> </th>
                 </tr>
                 <?php endforeach ?>
               </table>
