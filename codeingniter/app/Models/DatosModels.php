d502461d15b68af8bf66e7ece492f9c7b2f030af
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-06-10 09:30:25 -0500

    Update DatosModels.php

--- codeingniter/app/Models/DatosModels.php
+++ codeingniter/app/Models/DatosModels.php
@@ -1,1 +1,10 @@
+<?
+namespace App\Models;
+use CodeIgniter\Model;
 
+class DatosModel extends Model {
+     public fuction listar() {
+       $m = $this->db->query("SELECT * from persona");
+         return $m->getResult();
+     }
+}
