e7cc190ea196bb30a69d945ab13167618eeab0b8
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-06-17 11:47:07 -0500

    Update actualizar.php

--- codeingniter/app/Views/actualizar.php
+++ codeingniter/app/Views/actualizar.php
@@ -11,7 +11,23 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <h1>Actualizar registro</h1>
+      <div class="row">
+        <div class="col-sm-12">
+          <form method="POST" action="<?php echo base_url('/index.php/actualizar') ?>">
+            <input type="hidden" name="id" id="id" value="<?php echo $id ?>">
+            <label for="nombre">Nombre:</label>
+            <input type="text" class="form-control" name="nombre" id="nombre" value="<?php echo $nombre?>" required>
+            <label for="apaterno">Apellido paterno:</label>
+            <input type="text" name="apaterno" id="apaterno" class="form-control" value="<?php echo $apaterno?>" required>
+            <label for="apaterno">Apellido materno:</label>
+            <input type="text" name="amaterno" id="amaterno" class="form-control" value="<?php echo $amaterno?>" required>
+            <button class="btn btn-warning form-control">Editar</button>
+          </form>
+        </div>
+      </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
