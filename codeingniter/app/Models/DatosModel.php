7d2e20326d3d25209b701a7d4abb5e126a3612f6
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-06-17 10:16:22 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -3,8 +3,35 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class DatosModel extends Model {
-     public function listar() {
-       $m = $this->db->query("SELECT * from persona");
-         return $m->getResult();
+     public function obtenerInformacion($data){
+          $model = $this ->db->table('persona');
+          $model-> where($data);
+          return $model->getResult();
      }
+     
+    
+     public function listarTodo() {
+       $model = $this->db->query("SELECT * from persona");
+         return $model->getResult();
+     }
+     
+     public function insertar($data){
+          $model = $this ->db->table('persona');
+          $model-> insert($data);
+          return $this->db->insertID();
+     }
+     
+     public function actualizar($data, $id){
+          $model = $this ->db->table('persona');
+          $model->set($data);
+          $model->where($id);
+          return $model->update();
+     }
+     
+     public function eliminar($id){
+          $model = $this->db->table('persona');
+          $model -> where($id);
+          return $model -> delete();
+     }
+     
 }
