e795b722c4b3e225ecec92e2953c11cd1f39485c
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-06-18 12:46:26 -0500

    Update actualizar.php

--- codeingniter/app/Views/actualizar.php
+++ codeingniter/app/Views/actualizar.php
@@ -1,8 +1,8 @@
 <?php
 $id = $datos[0]['id'];
 $nombre = $datos[0]['nombre'];
-$apaterno = $datos [0][apellido_paterno];
-$amaterno = $datos [0][apellido_materno];
+$apaterno = $datos [0]['apellido_paterno'];
+$amaterno = $datos [0]['apellido_materno'];
 
 ?>
 
